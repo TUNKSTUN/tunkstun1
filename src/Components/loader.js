5d0ffe7ee64997291a1410b94eb205e5953478ec
TUNKSTUN <johnwick4learning@gmail.com>
2023-03-01 13:24:13 +0530

    ...

--- src/Components/loader.js
+++ src/Components/loader.js
@@ -23,8 +23,8 @@ const Loader = () => {
   }, []);
 
   return (
-    <div className={`font-mono p-10  text-center text-xl text-teal-300 bg-black flex flex-col w-full h-screen z-20 fixed top-0 left-0  justify-center items-center ${loading ? "block" : " transition ease-in-out duration-700 -translate-y-full "}`}>
-      <InfinitySpin color="	#AFEEEE" height={150} width={150} className="bg-white"/>
+    <div className={`font-mono p-10  text-center text-xl text-teal-300 bg-black flex  w-full h-full z-20 fixed top-0 left-0 pr-16 xl:pr-28 lg:pr-20 justify-center items-center ${loading ? "block" : " transition ease-in-out duration-700 -translate-y-full "}`}>
+      <InfinitySpin color="	#AFEEEE" height={120} width={120} className=""/>
     </div>
   );
 };
